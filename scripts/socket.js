20b506b509717a9e35f04f793f88977021d884aa
Benji Kay <okaybenji@gmail.com>
2016-06-10 14:44:48 -0500

    modify socket script to use cloud9 ip

--- scripts/socket.js
+++ scripts/socket.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket('ws://action-rpg-okaybenji.c9users.io:8080');
 
 ws.onmessage = function(data, flags) {
   const messageText = data.data;
