b64d1dde88d32b9c2e7da8525818e10b1c59834d
Benji Kay <okaybenji@gmail.com>
2016-06-18 23:48:17 -0500

    refactor client socket code

--- scripts/socket.js
+++ scripts/socket.js
@@ -8,43 +8,46 @@ const createSocket = function() {
     const msg = JSON.parse(data.data);
 //    console.log('received message:', msg);
 
-    switch (msg.type) {
-      case 'id':
+    const messageHandlers = {
+      id() {
         id = msg.id;
-        break;
-      case 'chat':
+      },
+      chat() {
         chat.log(msg.text);
-        break;
-      case 'spawn':
+      },
+      spawn() {
         if (msg.id === id) {
           player = players[msg.id] = createPlayer(game, {x: msg.x, y: msg.y, isClient: true});
         } else {
           players[msg.id] = createPlayer(game, {x: msg.x, y: msg.y});
         }
-        break;
-      case 'destroy':
-        if (players[msg.id]) {
-          players[msg.id].destroy();
-        }
-        console.log(msg.id, 'disconnected.');
-        break;
-      case 'move':
-        if (msg.id === id) { // us
+      },
+      move() {
+        if (msg.id === id) { // it's us!
           // if the client's position at last received time from server does not match server's
           // reported position, reconcile the player position
           if (player && !player.positionAtTimeMatchesServer(msg.time, msg.position)) {
             players[msg.id].syncPositionWithServer(msg.time, msg.position);
           }
+          // purge consumed history
           players[msg.id].clearInputHistoryBeforeTime(msg.time);
-        } else { // someone else
+        } else { // it's someone else
           // update position
           // TODO: interpolate over time
           // TODO: implement lag compensation (broadcast input histories and play them back in the past)
           players[msg.id].x = msg.position.x;
           players[msg.id].y = msg.position.y;
         }
-        break;
-    }
+      },
+      destroy() {
+        // TODO: test this -- it may not be working
+        if (players[msg.id]) {
+          players[msg.id].destroy();
+        }
+      },
+    };
+
+    messageHandlers[msg.type]();
   };
 
   return {
